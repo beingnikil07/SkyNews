fe39bc502afc169feeee66ee9fd04edf337227fb
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-16 11:05:34 +0530

    Fetching API keys from NewsApi

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,13 +6,15 @@ export default class NewsItem extends Component {
     //title ,description ko extract krr do
     return (
       <div>
-        <div className="card" style={{width:"18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://ichef.bbci.co.uk/news/1024/branded_news/1B6E/production/_128022070_e0404ba4040e9ebe45f075385066b8e3a804ecc70_642_6789_38211000x563.jpg"
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description}
-            </p>
+            <p className="card-text">{description}</p>
             <a href="/" className="btn btn-primary">
               Go somewhere
             </a>
