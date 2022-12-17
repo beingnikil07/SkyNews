1c94e88a8fdf534cc57a1be159cda071802e9f65
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-17 08:39:37 +0530

    Adding pagination in skynews

--- src/components/News.js
+++ src/components/News.js
@@ -1,35 +1,80 @@
-import React, { Component } from "react";
-import NewsItem from "./NewsItem";
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
-export default class News extends Component {
+export class News extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false
+    constructor(){
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1
+        }
     }
-  }
 
-  async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=a7b691c7393a4548a85aa775e8a94b31"
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    this.setState({ articles: parsedData.articles })
-  }
-  render() {
-    return (
-      <div className="container my-3">
-        <h1>Skynews-top headlines</h1>
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return <div className="col-md-4 my-2" key={element.url} >
+    async componentDidMount(){ 
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=a7b691c7393a4548a85aa775e8a94b31&page=1pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData); 
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    }
 
-              <NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 85)} imageUrl={element.urlToImage} newsUrl={element.url} />
+     handlePrevClick = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a7b691c7393a4548a85aa775e8a94b31&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);  
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
+    }
+    
+     handleNextClick = async ()=>{
+        console.log("Next"); 
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a7b691c7393a4548a85aa775e8a94b31&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);  
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+    }
+    }
+
+    render() { 
+        return (
+            <div className="container my-3">
+                <h1>NewsMonkey - Top Headlines</h1> 
+                <div className="row"> 
+                {this.state.articles.map((element)=>{
+                    return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    </div> 
+                })} 
+                </div> 
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
             </div>
-          })}
-        </div>
-      </div>
-    );
-  }
+        )
+    }
 }
+
+export default News
+
+
+
+
+
+
+
