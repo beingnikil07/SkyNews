55fd07d88207d31bc0da4810cd99263d6ef7278e
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-19 11:48:06 +0530

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React, { Component } from "react"; // if you are going to create a class based component then we have to add component from react always
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 class App extends Component {
   render() {
-     return (
+    return (
       <>
-        <Navbar />
-        <News pageSize={10} country="in" category="entertainment"/>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact key="general" path="/">
+              <News  pageSize={10} country="in" category="general" />
+            </Route>
+            <Route exact key="business" path="/business">
+              <News  pageSize={10} country="in" category="business" />
+            </Route>
+            <Route exact key="entertainment" path="/entertainment">
+              <News  pageSize={10} country="in" category="entertainment" />
+            </Route>
+            <Route exact key="general" path="/general">
+              <News  pageSize={10} country="in" category="general" />
+            </Route>
+            <Route exact key="health" path="/health">
+              <News  pageSize={10} country="in" category="health" />
+            </Route>
+            <Route exact key="science" path="/science">
+              <News  pageSize={10} country="in" category="science" />
+            </Route>
+            <Route exact key="sports" path="/sports">
+              <News  pageSize={10} country="in" category="sports" />
+            </Route>
+            <Route exact key="technology" path="/technology">
+              <News  pageSize={10} country="in" category="technology" />
+            </Route>
+          </Switch>
+        </Router>
       </>
     );
   }
 }
 export default App;
+
+
+
+// we are passing a unique key so that we can mount page automatically .
